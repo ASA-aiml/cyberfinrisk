d2c57286c101e12fbd093a0a2c6de65a7158317d
Shadil A M <shadilrayyan2@gmail.com>
2026-03-14 17:52:45 +0530

    removed the old frontend folder

--- old/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://yourdomain.com", // change to your domain
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: "daily",
-  priority: 0.7,
-};
